0fcd9a26a1f363acd8d6a88b9cfadd3fa196d739
zahid <syed.zahid.ali.sha@gmail.com>
2023-07-31 01:13:47 +0500

    Fixing bug in middleware settings

--- coin_sheet/settings.py
+++ coin_sheet/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware"
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.common.CommonMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
